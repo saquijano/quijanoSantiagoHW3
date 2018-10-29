8436640f36b4f079826385a6090cb584caaeb3ab
saquijano <sa.quijano@uniandes.edu.co>
2018-10-29 16:48:12 -0500

    creo que a esta pca

--- PCR.py
+++ PCR.py
@@ -6,27 +6,39 @@ from numpy.linalg import *
 preli=np.genfromtxt("WDBC.dat",delimiter=",",dtype='U16')
 filas=np.shape(preli)[0]
 colum=np.shape(preli)[1]
-datos=np.zeros((filas,colum-1))
-datosSexo=np.zeros((filas,colum))
+datos=np.zeros((filas,colum-2))
+datosMal=np.zeros((filas,colum))
+#numeros los meti a mano despues de contarlos con k y w
+benignos=np.zeros((357,np.shape(datos)[1]))
+malignos=np.zeros((212,np.shape(datos)[1]))
+
 hola1=preli[:,3]
-
+dondeB=[]
+dondeM=[]
 #converti la segunda columna en binaria. M =0 B=1
-
+k=0
+w=0
 for i in range(np.shape(preli)[0]):
 	for j in range(np.shape(preli)[1]):
 		if (j==0):
-			datosSexo[i,j]=float(preli[i,j])
-			datos[i,j]=int(preli[i,j])
+			datosMal[i,j]=float(preli[i,j])
 		elif (j==1):
 			if (preli[i,j]=='B'):
-				datosSexo[i,j]=1
+				datosMal[i,j]=1
+				benignos[k,:]=preli[i,j+1:]
+				k=k+1
 			else:
-				datosSexo[i,j]=0
+				datosMal[i,j]=0
+				malignos[w,:]=preli[i,j+1:]
+				w=w+1
 		else:
-			datosSexo[i,j]=float(preli[i,j])
-			datos[i,j-1]=float(preli[i,j])
+			datosMal[i,j]=float(preli[i,j])
+			datos[i,j-2]=float(preli[i,j])
+	
 
-print(np.shape(datos)[0],np.shape(datos)[1])
+print(malignos)
+print(w,k)
+print(len(malignos))
 
 hola=np.array([[1,2,1], [4,2,13],[7,8,1], [8,4,5]])
 chao=np.array([3,4])
@@ -54,6 +66,8 @@ matriz2=np.cov(datos)
 #cero=matriz-matriz2
 valores=np.linalg.eig(matriz)[0]
 vectores=np.linalg.eig(matriz)[1]
+
+# no lo uso, es evidente cuales son
 def factoresPri(valores,vectores):
 	temporal=0
 	temporal2=0
@@ -64,23 +78,30 @@ def factoresPri(valores,vectores):
 			temporal2=valores[i]		
 	return temporal,temporal2
 
-
-#print("valor propio principal, el 3:", valores[2])
+print("valor propio principal, el 1:", valores[0])
 print("Con vector propio:", vectores[:,0])
 
-#print("vectores propio secundario, el 2", valores[1])
+print("vectores propio secundario, el 2", valores[1])
 print("Con vector propio:", vectores[:,1])
 
 pEje=np.matmul(datos,vectores)
+benPlot=np.matmul(benignos,vectores)
+#benPlot2=np.matmul(benignos,vectores[1])
+
+malPlot=np.matmul(malignos,vectores)
+#malPlot2=np.matmul(malignos,vectores[1])
 princi=np.matmul(datos,vectores[2])
 
 plt.figure()
-plt.scatter(pEje[:,2],pEje[:,3])
+plt.title("PCA, primeros componentes")
+plt.scatter(benPlot[:,0],benPlot[:,1],label="benignos")
+plt.scatter(malPlot[:,0],malPlot[:,1],label="malignos")
+plt.show()
 
 plt.figure()
 plt.plot(princi)
 plt.title("otro")
-plt.show()
 
 
-print("El metodo de pca ayuda a reducir las variables que se usan para analizar un problema. Por esto es util en este caso, ya que, reduce la dimensionalidad de la variables pero permite el analisis del problema con estas")
+
+print("El metodo de pca ayuda a reducir las variables que se usan para analizar un problema. Por esto podria ser util en este caso, ya que, se hace un estudio con muchas variables. Sin embargo no parce que se separen mucho los grupos de beningno y maligno. Por lo cual puede que estos dos ejes no ayuden a clarificar el tipo de tumor.")
